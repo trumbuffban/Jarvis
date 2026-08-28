c9c73a0ab1610127aaf218657946f47721a342f3
Phamgiahung <phamgiahung@MacBook-Air-cua-Phamgiahung.local>
2026-08-29 01:17:53 +0700

    chore: reset agent data

--- src/langgraph_agent/set_up.py
+++ src/langgraph_agent/set_up.py
@@ -5,8 +5,7 @@ schedule = pd.DataFrame(columns= ['content'])
 schedule.index.name= 'date'
 schedule.to_csv(schedule_path)
 
-memory_path= Path(__file__).parent.parent.parent.resolve() / "data" / "memory.txt"
-if not memory_path.exists():
-    f = open(memory_path, 'w', encoding='utf-8')
-    f.write('Tôi là Jarvis, trợ lí AI của Hưng. Tôi được sinh ra vào ngày 25/08/2026')
-    f.close()
+memory_path = Path(__file__).parent.parent.parent.resolve() / "data" / "memory.txt"
+
+with open(memory_path, "w", encoding="utf-8") as f:
+    f.write("Tôi là Jarvis, trợ lí AI của Hưng. Tôi được sinh ra vào ngày 25/08/2026")
